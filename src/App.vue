5b81ed63373a36975a64d47c370ced99a5072784
JuliGo <juligo@gmail.com>
2026-04-18 16:04:48 -0300

    Página inicial do site

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+    <nav>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/sobre">Sobre</router-link> |
+        <router-link to="/contato">Contato</router-link> |
+        <router-link to="/login">Login</router-link> |
+        <router-link to="/dashboard">Dashboard</router-link>
+    </nav>
+
+    <router-view />
 </template>
 
 <style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import Home from '../views/Home.vue'
+
+const routes = [
+    { path: '/', component: Home }
+]
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes
 })
 
 export default router
