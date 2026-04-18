b26d0bc324c91e5f061331009855b4e62c81896f
JuliGo <juligo@gmail.com>
2026-04-18 16:09:27 -0300

    Página login

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Sobre from '../views/Sobre.vue'
 import Contato from '../views/Contato.vue'
+import Login from '../views/Login.vue'
 
 const routes = [
     { path: '/', component: Home },
     { path: '/sobre', component: Sobre },
     { path: '/contato', component: Contato },
+    { path: '/login', component: Login },
 ]
 
 const router = createRouter({
