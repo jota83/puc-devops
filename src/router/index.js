3443abd57159b38429c6c20b63725a2b973011e2
JuliGo <juligo@gmail.com>
2026-04-18 16:08:14 -0300

    Página contato

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '../views/Home.vue'
 import Sobre from '../views/Sobre.vue'
+import Contato from '../views/Contato.vue'
 
 const routes = [
     { path: '/', component: Home },
     { path: '/sobre', component: Sobre },
+    { path: '/contato', component: Contato },
 ]
 
 const router = createRouter({
---
+++ src/views/Contato.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+Página Contato
+</template>
+
+<style scoped>
+
+</style>
